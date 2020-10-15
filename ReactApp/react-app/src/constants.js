be8c7c35edf0504c5fe17fd52732db956a61a82b
Datta <datta.battase@gmail.com>
2020-10-15 16:56:43 +0530

    success

---
+++ ReactApp/react-app/src/constants.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+export const images = {
+    Rainy_Umbrella_ChaiBhajji: require("./images/Rainy_Umbrella_ChaiBhajji.jpg"),
+    Summer_Beach_ChilloutOnBeach: require("./images/Summer_Beach_ChilloutOnBeach.jpg"),
+    Winter_Sweter_SnowFall:require("./images/Winter_Sweter_SnowFall.jpg")
+}
+
